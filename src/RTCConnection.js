e0a3765ebf7f8ccc438ae5701f3ef754d7f859a7
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2020-10-29 05:22:03 +0000

    streaming added for both end of user

--- src/RTCConnection.js
+++ src/RTCConnection.js
@@ -5,12 +5,42 @@
 //Chrome --> webkitGetUserMedia
 //Firefox --> mozGetUserMedia
 //navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-navigator.mediaDevices.getUserMedia({video: true, audio: true})
+const Peer = require('simple-peer')
+
+
+navigator.mediaDevices.getUserMedia({video: true, audio: false})
 .then(stream => {
-  const video = document.getElementById('localStream')
+  playVideo(stream, 'localStream')
+  const p = new Peer({
+    initiator: location.hash ==='#1', trickle: false, stream
+  })
+
+  p.on('stream', friendStream => playVideo(friendStream, 'friendStream'))
+
+  p.on('connect', () => {
+    /* setInterval(() => {
+      p.send(Math.random())
+    }, 2000); */
+  })
+
+  p.on('data', data => console.log(data))
+
+  p.on('signal', token => {
+    document.getElementById('txtMySignal').textContent = JSON.stringify(token)
+    console.log(token)
+  })
+
+  document.getElementById('btnConnect').addEventListener('click', event => {
+    const friendsignal = JSON.parse(document.getElementById('txtFriendSignal').value)
+    p.signal(friendsignal)
+  })
+})
+.catch(error => console.log(error))
+
+function playVideo(stream, idVideo) {
+  const video = document.getElementById(idVideo)
   video.srcObject = stream
   video.onloadedmetadata = ()=>{
     video.play()
   }
-})
-.catch(error => console.log(error))
+}
